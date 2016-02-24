744c572d75b6b814876e8ff2c7dc4303d1761be7
Mozhdeh <gheini72@gmail.com>
2016-02-24 15:59:01 +0330

    Initial working django project

--- querysystem/queryservices/views.py
+++ querysystem/queryservices/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+	return render(request, 'home.html')
+
+
+def year_country_query(request):
+	return render(request, 'year-country-query.html')
+
+
+def year_sorted_output_query(request):
+	return render(request, 'year-sorted-output-query.html')
--- querysystem/querysystem/urls.py
+++ querysystem/querysystem/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     # url(r'^$', 'querysystem.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'queryservices.views.home'),
+    url(r'^services/year-country-query', 'queryservices.views.year_country_query'),
+    url(r'^services/year-sorted-output-query', 'queryservices.views.year_sorted_output_query'),
     url(r'^admin/', include(admin.site.urls)),
 ]
